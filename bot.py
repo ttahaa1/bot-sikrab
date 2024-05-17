6c9e009ecf033ca7d3c55cebc364c42b58d4ec27
speed <153678863+ttahaa1@users.noreply.github.com>
2024-05-17 22:51:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,6 @@ import random
 import os
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-from telethon.tl.functions.messages import GetHistoryRequest
-from telethon.tl.types import InputPeerChannel, PeerChannel
 from telebot import types
 import asyncio
 from datetime import datetime
@@ -13,12 +11,11 @@ from datetime import datetime
 bot_token = "6526333880:AAGQkRsEinZQAht1UmDSIHb-4iX5wA5DkGc"
 admin_id = 6264668799
 
-session_string = "1AZWarzoBuz1FZKtjXxk7BchfGJKr5TepbQs_ArIkG6u6pW8x083CU1CQLqAhVmsKYZM8RemNykBCmW7PXg2IoECv6w8VJSx6WgXxDQgYQxLrTi6tdMVDvXxo9LyxC5mEB7mfNpva7wc2ULRM9CkxZuBQD0Y0qL5ZJr8UJru6KDaIqeEL2OYH-o19pp5RXQz9l1ca6B8TKlP07oIXz-3l0ldN1fBZWPZZD9y_CxpKpMkmv0k4fp5bwvjEIRQiCp5bWhtURdXuViME9WLjZTHv4i6mUpoCMR3ZxTcrXgUj8pjZ8oqOaRCkHOvb8fe8GpJnZ9ggCDqx7naG3vPuzPgxVOQ0u-Fc3Wo="
-api_id = 27096137
-api_hash = "ae0b5ca26f7e604066666bd1e2bff182"
-channel_username = "N8GEH"
-
 async def get_last_messages(username, limit, bin):
+    session_string = "1AZWarzoBuz1FZKtjXxk7BchfGJKr5TepbQs_ArIkG6u6pW8x083CU1CQLqAhVmsKYZM8RemNykBCmW7PXg2IoECv6w8VJSx6WgXxDQgYQxLrTi6tdMVDvXxo9LyxC5mEB7mfNpva7wc2ULRM9CkxZuBQD0Y0qL5ZJr8UJru6KDaIqeEL2OYH-o19pp5RXQz9l1ca6B8TKlP07oIXz-3l0ldN1fBZWPZZD9y_CxpKpMkmv0k4fp5bwvjEIRQiCp5bWhtURdXuViME9WLjZTHv4i6mUpoCMR3ZxTcrXgUj8pjZ8oqOaRCkHOvb8fe8GpJnZ9ggCDqx7naG3vPuzPgxVOQ0u-Fc3Wo="
+    api_id = 27096137
+    api_hash = "ae0b5ca26f7e604066666bd1e2bff182"
+
     async with TelegramClient(StringSession(session_string), api_id, api_hash) as client:
         try:
             username = int(username)
@@ -60,26 +57,6 @@ async def get_last_messages(username, limit, bin):
         random.shuffle(matching_texts)
         return "\n".join(matching_texts), name
 
-async def get_random_image(client):
-    channel = await client.get_entity(channel_username)
-    result = await client(GetHistoryRequest(
-        peer=channel,
-        limit=100,  # Adjust the limit according to your needs
-        offset_date=None,
-        offset_id=0,
-        max_id=0,
-        min_id=0,
-        add_offset=0,
-        hash=0
-    ))
-    
-    photos = [message for message in result.messages if message.media]
-    if not photos:
-        return None
-    
-    random_photo = random.choice(photos)
-    return await client.download_media(random_photo)
-
 def save_to_file(text):
     if os.path.exists('combo.txt'):
         os.remove('combo.txt')
@@ -96,17 +73,17 @@ command_pattern = r'^(?:/|\.|)scr(?:\s+(.*))?$'
 def send_scr_messages(message):
     chat_id = message.chat.id
     language = 'en'
-    msg = """Scraping Started...⏳"""
+    msg = "Scraping Started...⏳"
     msg1 = ["Amount", "Took", "Source"]
-    msg2 = """command format. Use /scr [username] [limit]"""
+    msg2 = "command format. Use /scr [username] [limit]"
     if language == 'es':
-        msg = """Raspado iniciado...⏳"""
+        msg = "Raspado iniciado...⏳"
         msg1 = ["Cantidad", "Tomó", "Fuente"]
-        msg2 = """formato de comando. Utilice /scr [nombre de usuario] [límite]"""
+        msg2 = "formato de comando. Utilice /scr [nombre de usuario] [límite]"
     initial_message = bot.reply_to(message, msg)
     start_time = datetime.now()
     command_parts = message.text.split()
-    if len(command_parts) == 4 and command_parts[0].endswith('scr') or len(command_parts) == 3 and command_parts[0].endswith('scr'):
+    if (len(command_parts) == 4 and command_parts[0].endswith('scr')) or (len(command_parts) == 3 and command_parts[0].endswith('scr')):
         username = command_parts[1]
         limit = int(command_parts[2])
         try:
@@ -134,8 +111,10 @@ def send_scr_messages(message):
             try:
                 markup = types.InlineKeyboardMarkup()
                 team_button = telebot.types.InlineKeyboardButton(text="Dev Team", url='https://t.me/tcrep1')
-                dev_button = telebot.types.InlineKeyboardButton(text="Dev", url='https://t.me/KOK0KK')
-                markup.add(team_button, dev_button)
+                dev_button = telebot.types.InlineKeyboardButton(text="𝟭𝟭:𝟰𝟵 𓏺 ° مغلق °", url='https://t.me/KOK0KK')
+                speed_button = telebot.types.InlineKeyboardButton(text="ᔆ ᴾ ᴱ ᴱ ᴰ ™𝓼", url='https://t.me/l_s_I_I')
+                leo_button = telebot.types.InlineKeyboardButton(text="《 𝑳𝐒 》⏤͟͞ LEO𓆪", url='https://t.me/V_1_1_1_0')
+                markup.add(team_button, dev_button, speed_button, leo_button)
                 bot.send_document(message.chat.id, file, caption=captain_info, parse_mode='none', reply_markup=markup)
                 bot.delete_message(message_id=initial_message.message_id, chat_id=chat_id)
             except Exception as e:
@@ -156,7 +135,7 @@ def send_help_message(message):
     <b>مثال:</b>
     /scr xenscrape 100
 
-    إذا كنت بحاجة إلى مزيد من المساعدة، يرجى الاتصال بفريق التطوير. @KOK0KK
+      إذا كنت بحاجة إلى مزيد من المساعدة، يرجى الاتصال بفريق التطوير. @KOK0KK
     """
     bot.send_message(message.chat.id, help_text, parse_mode='html')
 
@@ -167,17 +146,7 @@ def send_welcome_message(message):
     <b>هذا البوت هو بوت كومبو يقوم بإنشاء كومبو من بين معين أو عشوائي.</b>
     للحصول على مزيد من المعلومات حول كيفية استخدام البوت، استخدم الأمر /help.
     """
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    random_photo = loop.run_until_complete(fetch_random_photo())
-    
-    if random_photo:
-        bot.send_photo(message.chat.id, random_photo, caption=welcome_text, parse_mode='html')
-    else:
-        bot.send_message(message.chat.id, welcome_text, parse_mode='html')
-
-async def fetch_random_photo():
-    async with TelegramClient(StringSession(session_string), api_id, api_hash) as client:
-        return await get_random_image(client)
+    video_url = "https://telegra.ph/file/f48b45d973150c4802594.mp4"
+    bot.send_video(message.chat.id, video_url, caption=welcome_text, parse_mode='html')
 
 bot.infinity_polling()
